2d870556fbef83b00b373ab5e18e5362d37e3a0e
Dean Mckendry <dean.mckendry91@gmail.com>
2018-10-30 11:00:18 +1100

    Wrong import

--- userdefinedfields/__init__.py
+++ userdefinedfields/__init__.py
@@ -1,5 +1,5 @@
 name = "django-user-defined-fields"
 
-VERSION = (0, 0, 2)
+VERSION = (0, 0, 3)
 
 __version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)]) # noqa
--- userdefinedfields/context_processors.py
+++ userdefinedfields/context_processors.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from models import DisplayCondition, ExtraField
+from userdefinedfields.models import DisplayCondition, ExtraField
 
 
 def get_extra_fields():
